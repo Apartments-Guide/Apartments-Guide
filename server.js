d919f1b799fb821ca8c67b6df6a695fb89561765
daliaawad <awaddalia60@gmail.com>
2022-05-13 02:17:44 +0300

    Save Log in values in the Database

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://software:software@cluster0.cv6lp.mongodb.net/apatmentsGuide", { useNewUrlParser: true}, {useUnifiedTopology: true})
+
+
+const userSchema = {
+    userType: Number,    //1 means landlord        2 means customer
+    email: String,
+    password: String, 
+}
+const User = mongoose.model("User", userSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.send("Express is working")
+    res.sendFile(__dirname + "/SignUp/sign_up.html")
 })
+
+app.post("/", function(req, res) {
+    let newUser = new User({
+        userType: req.body.userType,
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser.save();
+    res.redirect("/");
+})
+
 app.listen(3000, function() {
     console.log("Server is running on 3000");
 });
